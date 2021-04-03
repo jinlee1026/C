855a12f32d306043769e158804a1f3d44a70390c
JLR <r.maristina@gmail.com>
2021-04-03 23:39:55 +0900

    Pick a number

---
+++ pick a number.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+char (Y);
+char (N);
+int input;
+
+printf("pick a number from 0 to 10 please.\n");
+printf("insert : ");
+scanf("%d",&input);
+
+printf("\n--------------------------");
+printf("\nI want you to confirm, ");
+printf ("is this your number? --> %d\n",input);
+
+printf("choose Y or N\n");
+printf("insert : ");
+
+if (scanf("%s",&Y));
+printf("thank you for confirming:)\n");
+
+return 0;
+
+}
